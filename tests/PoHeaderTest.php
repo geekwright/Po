5165caa94cc85bb62c303aa7ef02f50852907842
geekwright <richard@geekwright.com>
2020-12-30 17:08:08 -0600

    Enable testing on latest PHP versions

--- tests/PoHeaderTest.php
+++ tests/PoHeaderTest.php
@@ -12,18 +12,10 @@ class PoHeaderTest extends \PHPUnit\Framework\TestCase
      * Sets up the fixture, for example, opens a network connection.
      * This method is called before a test is executed.
      */
-    protected function setUp()
+    protected function setUp():void
     {
         $this->object = new PoHeader;
         $this->object->buildDefaultHeader();
-    }
-
-    /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
-    protected function tearDown()
-    {
     }
 
     public function test__construct()
